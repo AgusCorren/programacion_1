96ebb6d2780a48edaa64bed603ed479416ee5f47
Gonzalo A. <albarracin.agn@gmail.com>
2025-04-07 22:34:43 -0300

    Version 0.0.2 - TP2

--- backend/app.py
+++ backend/app.py
@@ -2,16 +2,13 @@ from flask import Flask
 import os
 from dotenv import load_dotenv
 
-# Cargar variables del .env
 load_dotenv()
 
-# Crear app Flask
 app = Flask(__name__)
 
-# Registrar rutas
-from main.routes import register_routes
-register_routes(app)
+@app.route("/", methods=["GET"])
+def home():
+    return {"mensaje": "API de rotisería funcionando correctamente!"}
 
-# Ejecutar
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.getenv('PORT', 5000)))
+    app.run(debug=True, port=int(os.getenv('PORT', 7000)))
